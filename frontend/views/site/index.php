59c8ffd8b8e1db41f4d6cca88a4376b78c9d14f8
Nikita Ketov <nketov@bigmir.net>
2018-10-13 14:18:38 +0300

    12102018

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -50,10 +50,12 @@ $model = new Product();
         <div class="card">
             <div class="card-contur"></div>
             <div class="card-img"></div>
-            <div class="card-text">Люстры 19605 серии <br> "Босфор Crystal"</div>
+            <div class="card-text">
+                <span class="card-text-text">Люстры 19605 серии <br> "Босфор Crystal"</span><span class="card-price"><?= round(25000/($i+1),2) ?> грн</span></p>
+            </div>
             <div class="info_hover">
-                <button class="btn btn-primary">Добавить в корзину</button>
-                <p style="font-size: .7rem">Артикул: 40807-cl6-pla000-cp000</p>
+                <button class="btn btn-primary">ДОБАВИТЬ В КОРЗИНУ</button>
+                <p style="font-size: .7rem; font-weight: lighter">Артикул: 40807-cl6-pla000-cp000</p>
             </div>
         </div>
   <?php } ?>
