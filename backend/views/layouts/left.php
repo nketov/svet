6217689e2002cfccea39e379a6f3e54b19116374
Nikita Ketov <nketov@bigmir.net>
2018-10-16 13:19:45 +0300

    fix

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -5,7 +5,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="images/favicon-32x32.png" class="img-circle" alt="User Image"/>
+                <img src="/admin/images/favicon-32x32.png" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
                 <p>Светлоград</p>
