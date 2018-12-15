f60d22ae7f46089d9ad1614a9bd109efd424e02a
Nikita Ketov <nketov@bigmir.net>
2018-12-15 13:12:44 +0200

    fix

--- frontend/views/site/_article.php
+++ frontend/views/site/_article.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
 <img src="/images/articles/<?= $model->image_name ?>" alt="" style="width: 100%; margin-top: 25px;">
 <div class="article-preview">
     <h2><?= $model->header ?></h2>
-    <div><?= StringHelper::truncate($model->content, 500, '',null, true).'<br>'. Html::a('Читать дальше...',  Url::to('/article/'.$model->id)) ?></div>
+    <div><?= StringHelper::truncate($model->content, 500, '',null, true).'<br>'. Html::a('<b>Читать дальше...</b>',  Url::to('/article/'.$model->id)) ?></div>
 </div>
 
 
